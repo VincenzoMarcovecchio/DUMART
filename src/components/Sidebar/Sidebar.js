30689c7b440401d3b1fb0eab13c2328fffdff0c3
Vincenzo Marcovecchio <vinceoldmark@gmail.com>
2020-12-20 19:33:42 +0100

    fixed dots on sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -23,15 +23,15 @@ const Sidebar = ({ hideMobile }) => {
         <Categories />
         <Tags />
         <br />
-        <span> 1° posto concorso on - line “Ad Lucem” 2013 - Oubliette Magazine;</span>
+        <span> 1° posto concorso on - line “Ad Lucem” 2013 - Oubliette Magazine</span>
         <br />
         <br />
-        <span> Attestato d’Onore con targa nel Premio VIVARIUM 1012;</span>
+        <span> Attestato d’Onore con targa nel Premio VIVARIUM 1012</span>
         <br />
         <br />
         <span>
           Menzione d’Onore nella 8ª edizione del Premio Letterario Internazionale Trofeo
-          Penna d’Autore per la Poesia.
+          Penna d’Autore per la Poesia
         </span>
         <br />
         <br />
@@ -40,7 +40,7 @@ const Sidebar = ({ hideMobile }) => {
         <br />
         <span>
           1° posto assoluto nella VIII edizione Premio Internazionale di Poesia e
-          Narrativa Napoli Cultural Classic (sezione poesie a valore religioso).
+          Narrativa Napoli Cultural Classic (sezione poesie a valore religioso)
         </span>
         <br />
         <br />
@@ -51,14 +51,14 @@ const Sidebar = ({ hideMobile }) => {
         <br />
         <span>
           1° Posto assoluto nella XXII edizione Concorso Letterario Internazionale “MOI.
-          CA.” di Poesia e Narrativa.
+          CA.” di Poesia e Narrativa
         </span>
         <br />
         <br />
-        <span>1° posto nel concorso on-line PoesiaEPoeti 2011.</span>
+        <span>1° posto nel concorso on-line PoesiaEPoeti 2011</span>
         <br />
         <br />
-        <span>2° Posto Concorso di Poesia 2013 5° Raduno di Gocce di Poesia.</span>
+        <span>2° Posto Concorso di Poesia 2013 5° Raduno di Gocce di Poesia</span>
         <br />
         <br />
         <span>
@@ -67,46 +67,46 @@ const Sidebar = ({ hideMobile }) => {
         </span>
         <br />
         <br />
-        <span>3° Posto alla X edizione del Premio Letterario "Le Pieridi";</span>
+        <span>3° Posto alla X edizione del Premio Letterario "Le Pieridi"</span>
         <br />
         <br />
         <span>
           8° Posto alla XIII Edizione del Concorso Nazionale di Poesia “Il castello di
-          Sopramonte” Prato Sesia – NOVARA.
+          Sopramonte” Prato Sesia – NOVARA
         </span>
         <br />
         <br />
         <Link to="/poesia/vola-colomba-ai-rintocchi-del-vespro-rosso-sul-colle">
-          3° posto al “III Concorso Nazionale Gocce di Poesia 2012”.
+          3° posto al “III Concorso Nazionale Gocce di Poesia 2012”
         </Link>
         <br />
         <br />
         <Link to="/poesia/sogno-sul-mare-del-vasto">
-          Menzione d’Onore sezione haiku “III Concorso Nazionale Gocce di Poesia 2012”.
+          Menzione d’Onore sezione haiku “III Concorso Nazionale Gocce di Poesia 2012”
         </Link>
         <br />
         <br />
         <span>
           Menzione di merito nella IV edizione del Premio Letterario Internazionale “Città
-          di Martinsicuro” 2012 per la Poesia.
+          di Martinsicuro” 2012 per la Poesia
         </span>
         <br />
         <br />
         <span>
           Menzione d’Onore – Undicesima Edizione – 2012 - Premio Internazionale
-          “L’Arcobaleno della vita” - Città di Lendinara.
+          “L’Arcobaleno della vita” - Città di Lendinara
         </span>
         <br />
         <br />
         <Link to="poesia/il-tratto-di-un-popolo-migrante">
           Menzione d’Onore sezione haiku – Undicesima Edizione – 2012 - Premio
-          Internazionale “L’Arcobaleno della vita” - Città di Lendinara.
+          Internazionale “L’Arcobaleno della vita” - Città di Lendinara
         </Link>
         <br />
         <br />
         <Link to="poesia/cono-di-ombra">
           6° posto e pubblicazione su libro/enciclopedia “Le origini lucane di Antonio
-          Vivaldi” in ambito “Premio POMARICVM Antonio Vivaldi”.
+          Vivaldi” in ambito “Premio POMARICVM Antonio Vivaldi”
         </Link>
         <br />
         <br />
@@ -117,23 +117,23 @@ const Sidebar = ({ hideMobile }) => {
         <br />
         <br />
         <Link to="poesia/sguardi-sbiaditi">
-          menzione di merito III Concorso di Poesia Satirica "I Fiori del Bene";
+          menzione di merito III Concorso di Poesia Satirica "I Fiori del Bene"
         </Link>
         <br />
         <br />
         <Link to="poesia/un-amore-eterno">
           Finalista nel XXI premio letterario “Maria Scarcella Padovano” MOI. CA. Concorso
-          Internazionale di Poesia e Narrativa – 2011.
+          Internazionale di Poesia e Narrativa – 2011
         </Link>
         <br />
         <br />
         <Link to="poesia/navigando-in-un-mare-di-poesia">
           Finalista concorso on- line “San Valentino VS Faustino 2012” - Oubliette
-          Magazine.
+          Magazine
         </Link>
         <br />
         <br />
-        <span>Finalista nel concorso internazionale “L’otto milioni” - Ischia.</span>
+        <span>Finalista nel concorso internazionale “L’otto milioni” - Ischia</span>
       </div>
     </Sider>
   )
