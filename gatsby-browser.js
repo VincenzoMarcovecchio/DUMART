25e1375645417ae1f0da7190ba93a021d03b80a5
Vincenzo Marcovecchio <vinceoldmark@gmail.com>
2020-12-27 23:12:40 +0100

    change

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,2 @@
 // trigger an immediate page refresh when an update is found
-export const onServiceWorkerUpdateReady = () => window.location.reload()
+exports.onServiceWorkerUpdateReady = () => window.location.reload()
